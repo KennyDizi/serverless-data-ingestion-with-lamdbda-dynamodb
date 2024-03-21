b27f36be306a5b0eae2a6ea846d29a1916a175ae
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-03-21 21:17:16 +0700

    Prepare for ServerlessDataIngestionWithLamdbdaDynamodbStackProps

---
+++ lib/ServerlessDataIngestionWithLamdbdaDynamodbStackProps.ts
@@ -0,0 +1,20 @@
+import { StackProps } from "aws-cdk-lib";
+
+export interface ServerlessDataIngestionWithLamdbdaDynamodbStackProps extends StackProps {
+    /**
+     * A prefix used for naming resources to ensure uniqueness across deployments.
+     */
+    readonly resourcePrefix: string;
+    /**
+     * The AWS region where the resources will be deployed.
+     */
+    readonly deployRegion: string | undefined;
+    /**
+     * The environment (e.g., development, staging, production) for this deployment.
+     */
+    readonly deployEnvironment: string;
+    /**
+     * The name of the application. Used for resource naming and identification.
+     */
+    readonly appName: string;
+}
