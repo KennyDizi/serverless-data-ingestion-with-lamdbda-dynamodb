8e73d4f8a5a123dd8b30d82c4c2bb0e3b9836d08
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-03-21 20:12:31 +0700

    Init project

---
+++ test/serverless-data-ingestion-with-lamdbda-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessDataIngestionWithLamdbdaDynamodb from '../lib/serverless-data-ingestion-with-lamdbda-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-data-ingestion-with-lamdbda-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessDataIngestionWithLamdbdaDynamodb.ServerlessDataIngestionWithLamdbdaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
